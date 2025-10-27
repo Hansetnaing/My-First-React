f780135c538b8f3f64d61059e615144887ef60c3
Hansetnaing <minhansetnaing11@gmail.com>
2025-10-27 08:04:09 +0630

    adding image,display data and add css code

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-function AboutPage() {
+import { use } from "react";
+
+const user = {
+  name: "Roronoa Zoro",
+  imageUrl: "./public/luhpring.jpg",
+  imageSize: 90,
+};
+
+export default function App() {
   return (
     <>
-      <h1>About</h1>
-      <p>Hello there.<br />How do you do?</p>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={
+          {
+            width: user.imageSize,
+            height: user.imageSize
+          }
+        }
+        />
     </>
-  );
-}
-
-export default function myApp() {
-  return (
-    <div>
-      <h1>Welcome to My App</h1>
-      {AboutPage()}
-    </div>
   );
 }
