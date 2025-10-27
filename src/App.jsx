dd7117e321731110debfea0006dc1fcbed0fc1a1
Hansetnaing <minhansetnaing11@gmail.com>
2025-10-27 08:45:15 +0630

    Responding Event

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
-import { use } from "react"; 
+import { use } from "react";
 import './App.css';
 
-const products = [
-  {'title': 'Apple', 'id': 1},
-  {'title': 'Banana', 'id': 2},
-  {'title': 'Cherry', 'id': 3},
-]
-
-export default function ShoppingApp() {
-  const listItems = products.map((product) => (
-    <li key={product.id}>
-      {product.title}
-    </li>
-  ));
+export default function App() {
+  function handleClick() {
+    alert("Button clicked!");
+  }
 
   return (
-    <ul>{listItems}</ul>
-  );
+    <button onClick={handleClick}>
+      Click Me
+    </button>
+  )
 }
