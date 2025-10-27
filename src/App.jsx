84bcc77f8cda514c4e6a05661742fde5f52d50be
Hansetnaing <minhansetnaing11@gmail.com>
2025-10-27 09:27:55 +0630

    sharing data together and separately

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,39 @@ import { useState } from "react";
 import './App.css';
 
 export default function MyApp() {
+
+  const [count, setCount] = useState(0);
+  
+  function handleClick() {
+    setCount(count + 1);
+  }
+
   return (
-    <div>
-      <h1>Counters that update separately</h1>
-      <MyButton />
-      <MyButton />
-    </div>
+    <>
+      <div>
+        <h1>Counters that update separately</h1>
+        <MyButtonSeparate />
+        <MyButtonSeparate />
+      </div>
+
+      <div>
+        <h1>Counters that update together</h1>
+        <MyButton count={count} onClick={handleClick} />
+        <MyButton count={count} onClick={handleClick} />
+      </div>
+    </>
   );
 }
 
-function MyButton() {
+function MyButton({ count, onClick }) {
+  return (
+    <button onClick={onClick}>
+      Clicked {count} times
+    </button>
+  );
+}
+
+function MyButtonSeparate() {
   const [count, setCount] = useState(0);
 
   function handleClick() {
