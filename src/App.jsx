3b027ebb645823f60d89666476ebf1792a9913a9
Hansetnaing <minhansetnaing11@gmail.com>
2025-11-04 18:50:29 +0630

    Challenge 2 of 3: Adjust the image size based on a prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,127 +1,31 @@
-// not using props
-
-// import { getImageUrl } from './utils.jsx';
-
-// export default function Gallery() {
-//   return (
-//     <div>
-//       <h1>Notable Scientists</h1>
-//       <section className="profile">
-//         <h2>Maria Skłodowska-Curie</h2>
-//         <img
-//           className="avatar"
-//           src={getImageUrl('szV5sdG')}
-//           alt="Maria Skłodowska-Curie"
-//           width={70}
-//           height={70}
-//         />
-//         <ul>
-//           <li>
-//             <b>Profession: </b> 
-//             physicist and chemist
-//           </li>
-//           <li>
-//             <b>Awards: 4 </b> 
-//             (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)
-//           </li>
-//           <li>
-//             <b>Discovered: </b>
-//             polonium (chemical element)
-//           </li>
-//         </ul>
-//       </section>
-//       <section className="profile">
-//         <h2>Katsuko Saruhashi</h2>
-//         <img
-//           className="avatar"
-//           src={getImageUrl('YfeOqp2')}
-//           alt="Katsuko Saruhashi"
-//           width={70}
-//           height={70}
-//         />
-//         <ul>
-//           <li>
-//             <b>Profession: </b> 
-//             geochemist
-//           </li>
-//           <li>
-//             <b>Awards: 2 </b> 
-//             (Miyake Prize for geochemistry, Tanaka Prize)
-//           </li>
-//           <li>
-//             <b>Discovered: </b>
-//             a method for measuring carbon dioxide in seawater
-//           </li>
-//         </ul>
-//       </section>
-//     </div>
-//   );
-// }
-
-// using props
-
 import { getImageUrl } from './utils.jsx';
 
-function Profile({
-  imageId,
-  name,
-  profession,
-  awards,
-  discovery,
-  imageSize = 70
-}) {
+function Avatar({ person, size }) {
+
+  let thumbsize ='s';
+  if (size >= 90) {
+    thumbsize = 'b';
+  }
+
   return (
-    <section className="profile">
-      <h2>{name}</h2>
-      <img
-        className="avatar"
-        src={getImageUrl(imageId)}
-        alt={name}
-        width={imageSize}
-        height={imageSize}
-      />
-      <ul>
-        <li><b>Profession:</b> {profession}</li>
-        <li>
-          <b>Awards: {awards.length} </b>
-          ({awards.join(', ')})
-        </li>
-        <li>
-          <b>Discovered: </b>
-          {discovery}
-        </li>
-      </ul>
-    </section>
+    <img
+      className="avatar"
+      src={getImageUrl(person, thumbsize)}
+      alt={person.name}
+      width={size}
+      height={size}
+    />
   );
 }
 
-export default function Gallery() {
+export default function Profile() {
   return (
-    <div>
-      <h1>Notable Scientists</h1>
-      <Profile
-        imageId="szV5sdG"
-        name="Maria Skłodowska-Curie"
-        profession="physicist and chemist"
-        discovery="polonium (chemical element)"
-        awards={[
-          'Nobel Prize in Physics',
-          'Nobel Prize in Chemistry',
-          'Davy Medal',
-          'Matteucci Medal'
-        ]}
-      />
-      <Profile
-        imageId='YfeOqp2'
-        name='Katsuko Saruhashi'
-        profession='geochemist'
-        discovery="a method for measuring carbon dioxide in seawater"
-        awards={[
-          'Miyake Prize for geochemistry',
-          'Tanaka Prize'
-        ]}
-      />
-    </div>
+    <Avatar
+      size={40}
+      person={{ 
+        name: 'Gregorio Y. Zara', 
+        imageId: '7vQD0fP'
+      }}
+    />
   );
 }
-
